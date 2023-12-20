072d848fb43ba649d249183ca3ab6bd56035352e
Maurice MONEMOU <mauriceph1997monemou@gmail.com>
2023-12-20 17:20:23 +0100

    code ajouter

---
+++ indexEtudiants.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+	</style>
+	<title>Ex1</title>
+</head>
+<body>
+	<?php
+	$c = 1;
+	while ($c <= 10){
+		echo $c;
+		$c += 1;
+	}
+	?>
+</body>
+</html>
